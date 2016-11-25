20ee8667f20057037792b9ce10aa51960adb686b
Matthew Caldwell <caldwellysr@gmail.com>
2016-11-25 13:38:40 -0500

    strip boilerplate stuff and add first sprite for testing

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,5 @@
-/* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Crosshair from '../sprites/crosshair'
 import {setResponsiveWidth} from '../utils'
 
 export default class extends Phaser.State {
@@ -8,27 +7,18 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Phaser + ES6 + Webpack')
-    banner.font = 'Nunito'
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.anchor.setTo(0.5)
 
-    this.mushroom = new Mushroom({
+    this.crosshair = new Crosshair({
       game: this.game,
-      x: this.game.world.centerX,
-      y: this.game.world.centerY,
-      asset: 'mushroom'
+      x: 0,
+      y: 0,
+      asset: 'crosshair'
     })
 
-    // set the sprite width to 30% of the game width
-    setResponsiveWidth(this.mushroom, 30, this.game.world)
-    this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.crosshair, 0, 0);
+
   }
 
   render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
   }
 }
